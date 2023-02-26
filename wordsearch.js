a236e0984dbb8ffe275f0b72c3805aaeef441ec2
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-02-26 18:10:15 -0500

    Completed the implementation of the wordsearch function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
+// const letters = [
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ];
+
+
+
+
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
 
-module.exports = wordSearch
+
+    const verticalJoin = letters[0].map((col, index) =>
+    letters.map(row => row[index])
+  );
+
+    const verticalFlip = verticalJoin.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+      else {
+        for (l of verticalFlip) {
+          if (l.includes(word)) return true;
+        }
+      }
+    }
+    return false;
+
+};
+
+module.exports = wordSearch;
+
+
+
